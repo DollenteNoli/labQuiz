b9cdf866567b9155cb7f0df8333c91069e9eadc0
DollenteNoli <159417509+DollenteNoli@users.noreply.github.com>
2024-02-16 22:18:16 +0800

    added addition

--- Calculatorv2.java
+++ Calculatorv2.java
@@ -1,1 +1,22 @@
-new file GitHub
+public class Calculatorv2 {
+
+public static void main (String [] args) {
+
+//Addition
+  public static void Addition(){
+    int a = 10;
+    int b = 15;
+    int sum;
+    sum = a+b;
+    System.out.println("This is the sum of 2 numbers: " + sum);
+  }
+
+
+
+
+}
+
+
+
+
+}
